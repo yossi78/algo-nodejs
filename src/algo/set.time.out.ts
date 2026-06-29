ef8db027d19b22c639e389b71f92613c6bcfed8c
yossi78 <yossi78@gmail.com>
2026-06-29 19:55:54 +0300

    update set time out

--- src/algo/set.time.out.ts
+++ src/algo/set.time.out.ts
@@ -13,10 +13,10 @@ class SetTimeOut {
       // Operation 1: Synchronous (runs immediately)
       console.log("1. I run first because I am synchronous ֿ\n");
   
-      // Operation 2: Asynchronous (pushed to the Event Loop/Callback Queue)
+      // Operation 2: Asynchronous (pushed to the Event Loop/Callback Queue) // wait 3 seconds !!!!
       setTimeout(() => {
         console.log("2. I ran inside the setTimeout, so I was pushed to the end of the line \n");
-      }, 0);
+      }, 3000);
   
       // Operation 3: Synchronous (runs immediately after operation 1)
       console.log("3. I run immediately after the first synchronous operation \n");
