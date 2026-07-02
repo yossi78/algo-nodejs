20eebbd6b33556ef81ad2e382f3f71235b982e37
yossi78 <yossi78@gmail.com>
2026-07-02 11:00:07 +0300

    fix simple Promise

--- src/simple.promise.ts
+++ src/simple.promise.ts
@@ -2,29 +2,29 @@
 
 class SimplePromise {
 
-    public sum(a, b) : Promise<number> {
-        return Promise.resolve(a + b);
+    public addOne(a) : Promise<number> {
+        return Promise.resolve(a + 1);
     }
 
-    public mult(a, b): Promise<number> {
+    public addTwo(a): Promise<number> {
         return new Promise((resolve) => {
             setTimeout(() => {
-                resolve(a * b);
+                resolve(a + 2);
             }, 5000);
         });
     }
 
-    public async less(a, b) : Promise<number>{
-        return a - b;
+    public async addThree(a) : Promise<number>{
+        return a +3 ;
     }
 
-    public addOne(num) : number {
-        return num+1;
+    public addFour(num) : number {
+        return num+4;
     }
 
-    public addTwo(num: number): Promise<number> {
+    public addFive(num: number): Promise<number> {
         return new Promise((resolve) => {
-            resolve(num + 2);
+            resolve(num + 5);
         });
     }
 }
@@ -34,20 +34,20 @@ async function main() {
     const calculator = new SimplePromise();
 
     try {
-        const sumResult = await calculator.sum(10, 5);
-        console.log("Sum:", sumResult);
-
-        const multResult = await calculator.mult(10, 5);
-        console.log("Multiply:", multResult);
-
-        const addOne = calculator.addOne(10);
+        const addOne = await calculator.addOne(0);
         console.log("addOne:", addOne);
 
-        const lessResult = await calculator.less(10, 5);
-        console.log("Less:", lessResult);
-
-        const addTwo = await calculator.addTwo(10);
+        const addTwo = await calculator.addTwo(0);
         console.log("addTwo:", addTwo);
+
+        const addThree = await calculator.addThree(0);
+        console.log("addThree:", addThree);
+
+        const addFour = calculator.addFour(0);
+        console.log("addFour:", addFour);
+
+        const addFive = await calculator.addFive(0);
+        console.log("addFive:", addFive);
 
 
 
